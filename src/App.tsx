ff73556cdeab3fbdf64d4a50000a43d7e3efa35e
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-11 11:22:56 -0400

    moved Navbar to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import HomepageComponent from './components/home/HomepageComponent'
-
+import {HomepageComponent} from './components/home/HomepageComponent'
+import { Navbar } from './components/nav/Navbar'
 function App() {
 
   return (
     <>
+      <Navbar />
       <HomepageComponent />
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import { StudentComponent } from './components/student/StudentComponent.tsx'
+import { Navbar } from './components/nav/Navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route path='/' element={<App />} />
+      <Route path='/students' element={<StudentComponent />} />
     </Routes>
   </BrowserRouter>,
 )
