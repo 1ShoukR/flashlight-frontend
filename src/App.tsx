41bb1a354a67f72bd6f9e0ec953a6d2f4769957a
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-11 11:24:38 -0400

    removed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import {HomepageComponent} from './components/home/HomepageComponent'
-import { Navbar } from './components/nav/Navbar'
 function App() {
 
   return (
     <>
-      <Navbar />
-      <HomepageComponent />
+        <HomepageComponent />
     </>
   )
 }
