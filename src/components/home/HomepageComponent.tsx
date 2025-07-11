cee56e02e12a7f0f93705ebbee8626975101d967
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-11 10:46:37 -0400

    created homepage component

---
+++ src/components/home/HomepageComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class HomepageComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Homepage!</h1>
+            </div>
+        );
+    }
+}
